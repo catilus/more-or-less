9124586d83495c17b69a75e600806299d932cbb2
HelenePiet <helene.m.piet@gmail.com>
2020-09-28 21:07:29 -0700

    Initialization

---
+++ more-or-less/more-or-less/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  more-or-less
+//
+//  Created by Helene Piet on 9/28/20.
+//  Copyright © 2020 Helene Piet. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("More or less -- the game\n\n\n");
+    
+    return 0;
+}
