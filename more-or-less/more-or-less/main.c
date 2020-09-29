4705831efc1d4119d11854e7f9ba71bc7a11cb6b
HelenePiet <helene.m.piet@gmail.com>
2020-09-28 21:20:09 -0700

    Basic game coded

--- more-or-less/more-or-less/main.c
+++ more-or-less/more-or-less/main.c
@@ -12,7 +12,26 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("More or less -- the game\n\n\n");
     
-    return 0;
+    
+    const int MAX = 100, MIN = 1;
+    int mysteryNumber=0, inputNumber=0;
+    
+    srand(time(NULL));
+    mysteryNumber = (rand() % (MAX - MIN + 1)) + MIN;
+    
+    printf("More or less -- the game\n\n\n");
+    printf("Guess a number between 1 and 100:\n");
+    
+    while (inputNumber != mysteryNumber) {
+        scanf("%d", &inputNumber);
+    
+        if (inputNumber == mysteryNumber)
+            printf("You have guessed the mystery number!!!");
+        else if (inputNumber < mysteryNumber)
+            printf("It's more...\n");
+        else
+            printf("It's less...\n");
+    }
+        return 0;
 }
