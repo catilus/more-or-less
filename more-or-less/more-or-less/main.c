36a6f92f61ff16a4c511f92efb5ee4dd5964c052
HelenePiet <helene.m.piet@gmail.com>
2020-09-28 21:41:40 -0700

    Added the possibility to play another game

--- more-or-less/more-or-less/main.c
+++ more-or-less/more-or-less/main.c
@@ -12,30 +12,40 @@
 
 int main(int argc, const char * argv[]) {
     
-    // Initialize variables and constants
-    const int MAX = 100, MIN = 1;
-    int mysteryNumber=0, inputNumber=0, counter=0;
+    int keepPlaying=1;
     
-    // Random draw for mystery number
-    srand(time(NULL));
-    mysteryNumber = (rand() % (MAX - MIN + 1)) + MIN;
-    
-    printf("More or less -- the game\n\n\n");
-    printf("Guess a number between 1 and 100:\n");
-    
-    
-    while (inputNumber != mysteryNumber) {
-        scanf("%d", &inputNumber);
+    do {
         
-        counter++;
+        // Initialize variables and constants
+        const int MAX = 100, MIN = 1;
+        int mysteryNumber=0, inputNumber=0, counter=0;
         
-        if (inputNumber == mysteryNumber)
-            printf("You have guessed the mystery number in %d tries!!!", counter);
-        else if (inputNumber < mysteryNumber)
-            printf("It's more...\n");
-        else
-            printf("It's less...\n");
-        
-    }
+        // Random draw of mystery number
+        srand(time(NULL));
+        mysteryNumber = (rand() % (MAX - MIN + 1)) + MIN;
+    
+        printf("More or less -- the game\n\n\n");
+        printf("Guess a number between 1 and 100:\n");
+    
+    
+        while (inputNumber != mysteryNumber) {
+            scanf("%d", &inputNumber);
+            
+            counter++;
+            
+            if (inputNumber == mysteryNumber)
+                {
+                printf("You have guessed the mystery number in %d tries!!!\n", counter);
+                printf("To stop the game press [0], to play again press any number: ");
+                scanf("%d", &keepPlaying);
+                }
+            else if (inputNumber < mysteryNumber)
+                printf("It's more...\n");
+            else
+                printf("It's less...\n");
+            
+            }
+        } while (keepPlaying);
+    
         return 0;
 }
