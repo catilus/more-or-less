8819732e494b9648ebec75b36c4596fb8da510c9
HelenePiet <helene.m.piet@gmail.com>
2020-09-28 21:27:35 -0700

    Added counter feature

--- more-or-less/more-or-less/main.c
+++ more-or-less/more-or-less/main.c
@@ -11,27 +11,31 @@
 #include <time.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     
-    
+    // Initialize variables and constants
     const int MAX = 100, MIN = 1;
-    int mysteryNumber=0, inputNumber=0;
+    int mysteryNumber=0, inputNumber=0, counter=0;
     
+    // Random draw for mystery number
     srand(time(NULL));
     mysteryNumber = (rand() % (MAX - MIN + 1)) + MIN;
     
     printf("More or less -- the game\n\n\n");
     printf("Guess a number between 1 and 100:\n");
     
+    
     while (inputNumber != mysteryNumber) {
         scanf("%d", &inputNumber);
-    
+        
+        counter++;
+        
         if (inputNumber == mysteryNumber)
-            printf("You have guessed the mystery number!!!");
+            printf("You have guessed the mystery number in %d tries!!!", counter);
         else if (inputNumber < mysteryNumber)
             printf("It's more...\n");
         else
             printf("It's less...\n");
+        
     }
         return 0;
 }
